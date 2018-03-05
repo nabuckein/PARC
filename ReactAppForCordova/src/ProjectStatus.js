0e3cec530f1d6b313adb901d721472fc7c6fa26d
nabuckein <pauri@hotmail.com>
2018-03-04 21:22:27 -0600

    03/04//18

--- ReactAppForCordova/src/ProjectStatus.js
+++ ReactAppForCordova/src/ProjectStatus.js
@@ -11,23 +11,22 @@ class ProjectStatus extends Component{
 		this.state={
 			teamMembers:[]
 		}
+		
+	}
+	componentWillMount=(e)=>{
+		require("firebase/firestore");
 		var db = firebase.firestore();
 		var cityRef = db.collection('projects').doc('09493C'); 
 		var teamRef;
+		var that = this;
+		var teamMembersEls =[];
 		var getDoc = cityRef.get()
 			.then(doc => {
 				if (!doc.exists) {
 					console.log('No such document!');
 				} else {
-					console.log(doc.data().team);
 					teamRef = doc.data().team;
-					this.setState({teamMembers:teamRef});
-					/*
-					var ref =  doc.data().team[0];
-					ref.get().then(documentSnapshot => { //GET ENTIRE DOCUMENT IN THE REFERENCE
-						let data = documentSnapshot.data();
-						console.log(data);
-					});*/
+					//this.setState({teamMembers:teamRef});
 					/*
 					ref.get().then(documentSnapshot => { //GET ONLY FIRST NAME IN tHIs REFERENCE
 					if (documentSnapshot.exists) {
@@ -35,53 +34,86 @@ class ProjectStatus extends Component{
 					}
 					});*/
 				}
-			})
-			.catch(err => {
+			}).then(err=>{
+				for(var n=0; n<=teamRef.length-1; n++){
+					teamRef[n].get().then(documentSnapshot => { //GET ENTIRE DOCUMENT IN THE REFERENCE
+						let data = documentSnapshot.data();
+						teamMembersEls.push(data.firstName);
+						//console.log(data.firstName);
+					}).then(err=>{
+						this.setState({teamMembers:teamMembersEls});
+					});
+				}
+				
+			}).catch(err => {
 				console.log('Error getting document', err);
 			});
+			
+			
+			
+
 	}
-	componentWillMount=(e)=>{
-		require("firebase/firestore");
-		
+	componentDidMount=(e)=>{
 
 	}
 
 	
 
 	render(){
+		
+		console.log(this.state.teamMembers.length);
+		if(this.state.teamMembers.length>=0){
+			/*teamMembersEls =[];
+			var ref =  this.state.teamMembers;
+			for(var n=0; n<=ref.length-1; n++){
+				ref[n].get().then(documentSnapshot => { //GET ENTIRE DOCUMENT IN THE REFERENCE
+					let data = documentSnapshot.data();
+					teamMembersEls.push(<p>{data.firstName}</p>);
+					//console.log(data.firstName);
+				});
+			}
+				*/
+			//console.log(ref.firstName);
+			return(
+				<div>
+				<StyleRoot >
+					<div className="ProjectStatus" style={styles.ProjectStatus}>
+						<h1 style={styles.title}>{this.props.title}</h1>
+						
+						<div className="projectStatusAreaContainer" style={styles.projectStatusAreaContainer}>
 
+							<div style={styles.projectStatusArea}>
+								<h2 style={styles.projectStatusSecondaryTitle}>Redline History</h2>
+							</div>
 
-		return(
-			<div>
-			<StyleRoot >
-				<div className="ProjectStatus" style={styles.ProjectStatus}>
-					<h1 style={styles.title}>{this.props.title}</h1>
-					
-					<div className="projectStatusAreaContainer" style={styles.projectStatusAreaContainer}>
+							<div style={styles.projectStatusArea}>
+								<h2 style={styles.projectStatusSecondaryTitle}>Future Considerations</h2>
+							</div>
 
-						<div style={styles.projectStatusArea}>
-							<h2 style={styles.projectStatusSecondaryTitle}>Redline History</h2>
+							<div style={styles.projectStatusArea}>
+								<h2 style={styles.projectStatusSecondaryTitle}>Team</h2>
+								<p>{this.state.teamMembers[0]}</p>
+								<p>{this.state.teamMembers[1]}</p>
+							</div>
+
 						</div>
 
-						<div style={styles.projectStatusArea}>
-							<h2 style={styles.projectStatusSecondaryTitle}>Future Considerations</h2>
-						</div>
-
-						<div style={styles.projectStatusArea}>
-							<h2 style={styles.projectStatusSecondaryTitle}>Team</h2>
+						<div className="projectStatusButtonsContainer" style={styles.projectStatusButtonsContainer}>
+							<button className="projectStatusButtonCancel" key="projectStatusButtonCancel" style={styles.projectStatusButtonCancel} onClick={this.props.backToProjects}>CANCEL</button>
 						</div>
 
 					</div>
-
-                    <div className="projectStatusButtonsContainer" style={styles.projectStatusButtonsContainer}>
-						<button className="projectStatusButtonCancel" key="projectStatusButtonCancel" style={styles.projectStatusButtonCancel} onClick={this.props.backToProjects}>CANCEL</button>
-		            </div>
-
+					
+				</StyleRoot>
 				</div>
-				
-			</StyleRoot>
-			</div>
-		)
+			)
+		}
+		else {
+		//teamMembersEls.push(<p key="loading">Loading</p>);
+			return (
+				null
+			)
+		}
 	}
 }
 
